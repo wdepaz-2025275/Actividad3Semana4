5746afe2abd604391f4f3367832399640ed286d2
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-09 08:26:08 -0600

    fix: Se modifico el index del Backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,17 +1,28 @@
 import express from 'express';
 import cors from 'cors';
-import { obtenerProductos, validarProducto } from './controllers/carrito.controller';
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
+
 app.use(express.json());
 
-// Rutas de la API
-app.get('/api/productos', obtenerProductos);
-app.post('/api/validar-producto', validarProducto);
+const catalogo = [
+  { id: 1, nombre: 'Laptop Dell', precio: 4500, cantidad: 1 },
+  { id: 2, nombre: 'Mouse Inalámbrico', precio: 150, cantidad: 1 },
+  { id: 3, nombre: 'Teclado Mecánico', precio: 350, cantidad: 1 },
+    { id: 4, nombre: 'Bocinas Bluetooth', precio: 250, cantidad: 1 },
+    { id: 5, nombre: 'Mouse pad', precio: 50, cantidad: 1 },
+    { id: 6, nombre: 'Camaras para PC', precio: 100, cantidad: 1 }
+];
 
-app.listen(3000, () => {
-    console.log("Backend ejecutándose en http://localhost:3000");
+app.get('/api/productos', (req, res) => {
+  res.json(catalogo);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Node.js corriendo en http://localhost:${PORT}`);
 });
