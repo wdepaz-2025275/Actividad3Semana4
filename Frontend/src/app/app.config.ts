5ce04eddbe2ade2bbd3aef960fda483ee1b8bb5d
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-09 08:25:41 -0600

    fix: Se modifico el app.config del frontend

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
